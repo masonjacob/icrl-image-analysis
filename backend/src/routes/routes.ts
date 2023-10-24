42cf43a4ea6be43211d32ef64eee17c9d9b05325
Mason Jacob <masonjacob@uri.edu>
2023-10-23 23:02:53 -0400

    Updated backend to use multer middlware for FormData, not tested

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,12 +1,27 @@
-import express, { Router } from 'express'; // Import 'express' and the 'Router' type
+import express, { Router } from 'express';
+import multer, { Multer } from 'multer'; // Import 'multer'
 import { createRecord, findAllRecords, findRecordById, updateRecord, deleteRecord, listDatabaseInfo } from '../controllers/base.controller';
 import db from '../models'; // Import your Sequelize models
 
-const router: Router = express.Router(); // Create a router instance
+const router: Router = express.Router();
 
 // Define the ImageAnnotation and Image models
 const ImageAnnotation = db.image_annotations;
 const ImageModel = db.image;
+
+// Set up multer for file uploads
+const upload: Multer = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      // Define the directory where uploaded files will be stored
+      cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+      // Define the filename for the uploaded file
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 // Create routes using the BaseController functions
 router.post('/image-annotations', createRecord({ model: ImageAnnotation }));
@@ -16,7 +31,7 @@ router.put('/image-annotations/:id', updateRecord({ model: ImageAnnotation }));
 router.delete('/image-annotations/:id', deleteRecord({ model: ImageAnnotation }));
 
 // Add more routes for the ImageModel if needed
-router.post('/images', createRecord({ model: ImageModel }));
+router.post('/images', upload.single('image'), createRecord({ model: ImageModel })); // Use 'upload.single' middleware
 router.get('/images', findAllRecords({ model: ImageModel }));
 router.get('/images/:id', findRecordById({ model: ImageModel }));
 router.put('/images/:id', updateRecord({ model: ImageModel }));
@@ -24,4 +39,4 @@ router.delete('/images/:id', deleteRecord({ model: ImageModel }));
 
 router.get('/database-info', listDatabaseInfo);
 
-export default router; // Export the router
+export default router;
