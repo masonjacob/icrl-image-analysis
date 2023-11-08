becaedbb533c027de2d8fd9f88eebea0b560dfc0
Mason Jacob <masonjacob@uri.edu>
2023-11-08 18:21:16 -0500

    modified routes.ts

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -4,25 +4,10 @@ import db from '../models'; // Import your Sequelize models
 import upload from "./../config/multer.config";
 
 const router: Router = express.Router();
-const router: Router = express.Router();
 
 // Define the ImageAnnotation and Image models
 const ImageAnnotation = db.image_annotations;
 const ImageModel = db.image;
-
-// Set up multer for file uploads
-const upload: Multer = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      // Define the directory where uploaded files will be stored
-      cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-      // Define the filename for the uploaded file
-      cb(null, file.originalname);
-    },
-  }),
-});
 
 // Create routes using the BaseController functions
 router.post('/image-annotations', createRecord({ model: ImageAnnotation }));
@@ -41,5 +26,4 @@ router.delete('/images/:id', deleteRecord({ model: ImageModel }));
 
 router.get('/database-info', listDatabaseInfo);
 
-export default router;
 export default router;
